e39d1f8107dcc944b510812426646eb3f4ad258c
Hisam Fahri <me@hisamafahri.com>
2022-02-07 11:18:17 +0700

    feat(rgb2hsl): add rgb2hsl function

--- rgb/rgb2hsl.go
+++
@@ -1,5 +0,0 @@
-package coco
-
-func Rgb2Hsl() string {
-	return "rgb2hsl"
-}
---
+++ rgb2hsl.go
@@ -0,0 +1,54 @@
+package coco
+
+import (
+	"math"
+)
+
+func Rgb2Hsl(r float64, g float64, b float64) [3]float64 {
+	rVal := r / 255
+	gVal := g / 255
+	bVal := b / 255
+
+	tempMin := math.Min(rVal, gVal)
+	min := math.Min(tempMin, bVal)
+
+	tempMax := math.Max(rVal, gVal)
+	max := math.Max(tempMax, bVal)
+
+	delta := max - min
+
+	var h float64
+	var s float64
+
+	if max == min {
+		h = 0
+	} else if rVal == max {
+		h = (gVal - bVal) / delta
+	} else if gVal == max {
+		h = 2 + (bVal-rVal)/delta
+	} else if bVal == max {
+		h = 4 + (rVal-gVal)/delta
+	}
+
+	h = math.Min(h*60, 360)
+
+	if h < 0 {
+		h += 360
+	}
+
+	l := (min + max) / 2
+
+	if max == min {
+		s = 0
+	} else if l <= 0.5 {
+		s = delta / (max + min)
+	} else {
+		s = delta / (2 - max - min)
+	}
+
+	var result [3]float64
+	result[0] = math.Round(h)
+	result[1] = math.Round(s * 100)
+	result[2] = math.Round(l * 100)
+	return result
+}
